a68b885fdae37b42bd0e54380f1a4d8ebf39275f
rainbru <devnull@localhost>
2010-07-07 09:06:29 +0000

    v0.0.5-203: Added new test files.

---
+++ rainbrurpg/src/tests/ogreGui/WidgetListTest.hpp
@@ -0,0 +1,75 @@
+/*
+ *  Copyright 2006-2010 Jerome PASQUIER
+ * 
+ *  This file is part of RainbruRPG.
+ *
+ *  RainbruRPG is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  RainbruRPG is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with RainbruRPG; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ *  02110-1301  USA
+ *
+ */
+
+#ifndef WIDGET_LIST_TEST_H
+#define WIDGET_LIST_TEST_H
+
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/portability/Stream.h>
+
+#include <string>
+#include <sstream>
+#include <iostream>
+
+#include <WidgetList.hpp>
+
+using namespace std;
+using namespace RainbruRPG::OgreGui;
+
+/** A test case for ogreGui's Drawable
+  *
+  */
+class WidgetListTest : public CPPUNIT_NS::TestFixture 
+{
+  CPPUNIT_TEST_SUITE( WidgetListTest );
+
+  CPPUNIT_TEST( testNew );
+  CPPUNIT_TEST( testEmpty );
+  CPPUNIT_TEST( testPushBack );
+  CPPUNIT_TEST( testUniqueName );
+  CPPUNIT_TEST( testFocusNext );
+  CPPUNIT_TEST( testFocusPrevious );
+  CPPUNIT_TEST( testFocusMixed );
+
+  /// The CppUnit test end macro
+  CPPUNIT_TEST_SUITE_END();
+
+protected:
+  /** An instance of the caption widget
+    *
+    */
+  WidgetList* m_instance;
+  
+public:
+  void setUp();
+  void tearDown();
+
+  void testNew();
+  void testEmpty();
+  void testPushBack();
+  void testUniqueName();
+  void testFocusNext();
+  void testFocusPrevious();
+  void testFocusMixed();
+};
+
+#endif // WIDGET_LIST_TEST_H
