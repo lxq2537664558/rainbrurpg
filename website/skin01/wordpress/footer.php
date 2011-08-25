8e168286945058ca058594ab212f6dbf84c7419e
rainbru <devnull@localhost>
2011-08-25 19:54:00 +0000

    Add a footer to WP theme

---
+++ website/skin01/wordpress/footer.php
@@ -0,0 +1,32 @@
+</div><!-- #main -->
+<div id="rb-skin01-footer">
+
+			<?php
+				/* A sidebar in the footer? Yep. You can can customize
+				 * your footer with three columns of widgets.
+				 */
+				get_sidebar( 'footer' );
+			?>
+
+
+  Copyrights 2007-2011 The RainbruRPG contributors.
+
+  <span id="rb-skin01-footer-right">
+  <a href="http://sourceforge.net/projects/rainbrurpg">
+    <img src="http://sflogo.sourceforge.net/sflogo.php?group_id=193011&amp;type=12" 
+    alt="Get RainbruRPG at SourceForge.net. Fast, secure and Free Open Source software downloads" 
+    height="30" width="120"></a>
+  </span>
+
+  <span id="rb-skin01-footer-right">
+	<a href="http://wordpress.org/">
+	<img src="wp-content/themes/rainbrurpg/images/smbutton-blue.png" 
+	alt="Powered by Wordpress" height="30" width="25">
+	</a>
+  </span>
+  <div id="clear">&nbsp;</div>
+</div>
+
+<?php wp_footer(); ?>
+</body>
+</html>
