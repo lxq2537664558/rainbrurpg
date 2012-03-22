cb39d857d37b0db6a4417fbe18e1626cf3766f7b
rainbru <devnull@localhost>
2012-03-22 19:48:16 +0000

    Add support for Universal Edit Button to the custom Wordpress theme

--- website/skin01/wordpress/functions.php
+++ website/skin01/wordpress/functions.php
@@ -18,7 +18,29 @@ function rainbrurpg_setup()
 
 }
 
+function ueb_add_head_link() {
+  // Add support for the Universal Edit Button
+  // see http://universaleditbutton.org/Wordpress_plugin
+  if( is_single() || is_page() ) {
+    global $post;
+    switch ($post->post_type) {
+    case 'attachment':
+      return;
+    case 'page':
+      if(!current_user_can( 'edit_page', $post->ID ) ) return;
+      break;
+    case 'post':
+      //    default:
+      if (!current_user_can( 'edit_post', $post->ID ) ) return;
+      break;
+    }
+    
+    echo '<link rel="alternate" type="application/x-wiki" title="Edit this page" href="' . get_edit_post_link($post->ID) . "\" />\n";
+  }
+}
+
+
 add_filter( 'wp_page_menu_args', 'rainbrurpg_page_menu_args' );
 add_action( 'after_setup_theme', 'rainbrurpg_setup' );
-
+add_action( 'wp_head', 'ueb_add_head_link' );
 ?>
