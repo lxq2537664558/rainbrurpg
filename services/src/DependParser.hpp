518bd5123f538f4932f6137a9aae40a9906bac57
Jérôme Pasquier <rainbru@free.fr>
2013-10-06 23:27:09 +0200

    Tested a bit more boost::spirit::qi

--- services/src/DependParser.hpp
+++ services/src/DependParser.hpp
@@ -1,27 +1,43 @@
 #include <boost/spirit/include/classic.hpp>
-#include <boost/spirit/include/qi_grammar.hpp>
+#include <boost/spirit/include/qi.hpp>
 
-struct Syntax :
-  public boost::spirit::qi::grammar<Syntax>
+// see http://www.boost.org/doc/libs/1_54_0/libs/spirit/doc/html/spirit/qi/tutorials/employee___parsing_into_structs.html
+
+using namespace boost::spirit;
+
+struct depend
 {
-public:
-    Syntax( CParser &parser );
-    virtual ~Syntax();
-    template <typename ScannerT>
-    struct definition
+    int age;
+    std::string surname;
+    std::string forename;
+    double salary;
+};
+
+
+template <typename Iterator>
+struct depend_parser : qi::grammar<Iterator, depend(), ascii::space_type>
+{
+    depend_parser() : depend_parser::base_type(start)
     {
-    public:
-        definition( Syntax const &self )
-        {
-	  integer = lexeme[ +digit ];
-        }
-        boost::spirit::symbols<int> vars;
-        boost::spirit::rule<ScannerT> integer, factor, term,
-            expression, assignment, var_decl, declaration,
-            baseExpression;
-        const boost::spirit::rule<ScannerT> &start() const { return baseExpression; }
-    };
-    friend struct definition;
-private:
-    Private::FinishedProcessing *m_finishedProcessing;
+        using qi::int_;
+        using qi::lit;
+        using qi::double_;
+        using qi::lexeme;
+        using ascii::char_;
+
+        quoted_string %= lexeme['"' >> +(char_ - '"') >> '"'];
+
+        start %=
+            lit("employee")
+            >> '{'
+            >>  int_ >> ','
+            >>  quoted_string >> ','
+            >>  quoted_string >> ','
+            >>  double_
+            >>  '}'
+            ;
+    }
+
+    qi::rule<Iterator, std::string(), ascii::space_type> quoted_string;
+    qi::rule<Iterator, depend(), ascii::space_type> start;
 };
--- services/src/depend-parser-test.cpp
+++ services/src/depend-parser-test.cpp
@@ -1,24 +1,26 @@
-#include <algorithm>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
 #include <iostream>
-
-#include <boost/spirit/include/classic.hpp>
+#include <string>
+#include <algorithm>
 
 #include "DependParser.hpp"
 
 using namespace boost::spirit;
 using namespace std;
-
+using namespace boost::spirit::classic;
 int main(int argc, char **argv)
 {
   const string input("WikiName > 3.15");
-    vector<double> values;
-    Synthax parser;
+    /**
     parse_info<string::const_iterator> pInfo = parse(input.begin(), 
 						     input.end(),
-                                                     parser
+                                                     depend_parser
                                                     );
+    **/
+    parse_info<string::const_iterator> pInfo = p
+      qi::phrase_parse(input.begin(), input.end(), depend_parser);
 
-    copy(values.begin(), values.end(), ostream_iterator<double>(cout, "\n"));
         
     return 0;
 }
