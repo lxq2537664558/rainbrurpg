46b89888f6220bcd25dec94115588c9df5e05649
Jérôme Pasquier <rainbru@free.fr>
2014-08-09 11:08:09 +0200

    Create a cusstom guile console to test the api

---
+++ api/src/main.cpp
@@ -0,0 +1,22 @@
+#include <libguile.h>
+
+static void*
+register_functions (void* data)
+{
+  return NULL;
+}
+
+int main(int argc, char** argv)
+{
+  scm_with_guile (&register_functions, NULL);
+
+  // Here we must add logger, services, etc... modules
+
+  // logger library low level dynamic linking
+  SCM lib =  scm_dynamic_link (scm_from_locale_string ("./logger.so"));
+  scm_dynamic_call (scm_from_locale_string ("scm_init_rainbrurpg_logger_module"), lib);
+
+  scm_shell (argc, argv); 
+ return 0;
+}
+
--- logger/api/logger.i
+++ logger/api/logger.i
@@ -4,5 +4,19 @@
 
 #include "../src/Logger.hpp"
   //static const int MyVariable = 21;
+
+  void log_i(void)
+  {
+	     //
+
+  }
 %}
 %scheme %{ (export logger) %}
+
+/*
+../src/LoggerDef.hpp         ../src/LoggerOutputYaml.hpp
+../src/Logger.hpp            ../src/Mainpage.hpp
+../src/LoggerOutputFile.hpp  ../src/ObjectAttribute.hpp
+../src/LoggerOutput.hpp      ../src/Object.hpp
+../src/LoggerOutputTty.hpp   ../src/ObjectInspector.hpp
+*/
