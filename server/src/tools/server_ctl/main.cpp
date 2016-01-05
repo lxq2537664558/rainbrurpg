01b58b176c90535d9e0df91029f94968d7c1992f
Jérôme Pasquier <rainbru@free.fr>
2016-01-05 21:37:40 +0100

    Change headers date for 2016 in server/ and services/

--- server/src/tools/server_ctl/main.cpp
+++ server/src/tools/server_ctl/main.cpp
@@ -1,7 +1,7 @@
 /* 
  * server - The RainbruRPG's server binary.
  *
- * Copyright (C) 2011-2012, 2014-2015 Jérôme Pasquier
+ * Copyright (C) 2011-2012, 2014-2016 Jérôme Pasquier
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
