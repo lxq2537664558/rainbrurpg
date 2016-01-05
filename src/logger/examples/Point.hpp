7da78739eb21e68541d2782af79b97d89d847b73
Jérôme Pasquier <rainbru@free.fr>
2016-01-05 22:23:03 +0100

    Headers date for 2016 changed

--- src/logger/examples/Point.hpp
+++ src/logger/examples/Point.hpp
@@ -1,7 +1,7 @@
 /* 
  * logger - The RainbruRPG's logging library.
  *
- * Copyright (C) 2011-2012,2015 Jérôme Pasquier
+ * Copyright (C) 2011-2012,2015-2016 Jérôme Pasquier
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
