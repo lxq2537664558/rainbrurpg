74f6fbc5b94658c0421262cd1b225af350f96feb
Jérôme Pasquier <rainbru@free.fr>
2016-09-01 05:47:56 +0200

    Trying to install clang 3.9 on CI

--- CI/before_install.sh
+++ CI/before_install.sh
@@ -3,8 +3,12 @@
 # Ogre isn in universe
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
 
+# Install recent clang release to deal with a C++11/Boost/clang issue
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository "deb http://apt.llvm.org/precise/ llvm-toolchain-precise-3.9 main"
+
 sudo apt-get update -qq
 
 sudo apt-get install libogre-dev libcegui-mk2-dev libois-dev \
      libboost-program-options-dev libboost-system-dev libboost-filesystem-dev \
-     libboost-graph-dev libboost-test-dev libenet-dev guile-2.0-dev
+     libboost-graph-dev libboost-test-dev libenet-dev guile-2.0-dev clang-3.9
