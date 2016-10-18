03a6afd0b3cada58688bf0bf438bd3699ae80d65
Jérôme Pasquier <rainbru@free.fr>
2016-10-19 00:25:10 +0200

    Add basic manual CEGUI installation to CI

--- CI/before_install.sh
+++ CI/before_install.sh
@@ -1,23 +1,31 @@
-#!/bin/sh   
+#!/bin/sh -e  
 
 # Ogre is in universe
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
 # For clang-3.8
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu/ trusty-updates main universe"
 
-# Install recent clang release to deal with a C++11/Boost/clang issue
- #       sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
- #   sudo add-apt-repository "deb http://apt.llvm.org/precise/ llvm-toolchain-precise-3.8 main"
- #   wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
- #   sudo apt-get --yes --force-yes clang-3.8 lldb-3.8 
-
 sudo apt-get update -qq
 
 # Here I use --force-yes due to llvm signature errors
-sudo apt-get --yes install libogre-1.8-dev libcegui-mk2-dev libois-dev \
+sudo apt-get --yes install libogre-1.8-dev libois-dev mercurial \
      libboost-program-options-dev libboost-system-dev libboost-filesystem-dev \
      libboost-graph-dev libboost-test-dev libenet-dev guile-2.0-dev \
-     clang-3.8 lldb-3.8 libgtest-dev google-mock
+     clang-3.8 lldb-3.8 libgtest-dev google-mock 
+
+# Install CEGUI-0 and CEGUI-0-OGRE
+hg clone https://bitbucket.org/cegui/cegui cegui-source
+cd cegui-source/
+hg update -C v0-8
+mkdir build
+cd build/
+cmake ..
+make
+sudo make install
+
+## Handle linking from /usr/local
+sudo echo "/usr/local/lib" > /etc/ld.so.conf.d/cegui.conf
+sudo ldconfig
 
 # build libgtest & libgtest_main
 sudo mkdir /usr/src/gtest/build
