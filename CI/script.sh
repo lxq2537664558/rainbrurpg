2d4dcfbe087b30b39f98ea65cc2d7f778e8f5807
Jérôme Pasquier <rainbru@free.fr>
2016-11-09 03:58:14 +0100

    Remove rvm reference in allow_failures

--- CI/script.sh
+++ CI/script.sh
@@ -3,8 +3,8 @@
 # The '-e' flag in the shebang should make fail the script if
 # an error occurs.
 
-export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
-sudo ldconfig
+#export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
+#sudo ldconfig
 
 mkdir ./build
 cd build
