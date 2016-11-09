bf94643ecbb75d1502459ee4d63cdd4af3d7589d
Jérôme Pasquier <rainbru@free.fr>
2016-11-09 05:18:20 +0100

    CI allow_failure fix and concept work

--- CI/script.sh
+++ CI/script.sh
@@ -3,9 +3,6 @@
 # The '-e' flag in the shebang should make fail the script if
 # an error occurs.
 
-#export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
-#sudo ldconfig
-
 mkdir ./build
 cd build
 cmake ..
