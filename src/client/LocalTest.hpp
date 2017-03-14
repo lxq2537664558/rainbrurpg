31262c7dab397b5f9a25b6a6c741d55068180220
Jérôme Pasquier <rainbru@free.fr>
2017-03-14 15:07:53 +0100

    Add LocalTest empty class

---
+++ src/client/LocalTest.hpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2011-2017 Jerome Pasquier
+ *
+ * This file is part of rainbrurpg-client.
+ *
+ * rainbrurpg-client is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rainbrurpg-client is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rainbrurpg-client.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef _LOCACL_TEST_HPP_
+#define _LOCACL_TEST_HPP_
+
+#endif  // !_LOCACL_TEST_HPP_
